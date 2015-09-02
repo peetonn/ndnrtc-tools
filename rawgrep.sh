b0364afc689f1e326923d13b79b494a34a3c12e2
peetonn <gpeetonn@gmail.com>
2015-09-02 11:45:27 -0700

    new time-diff script in python (waaaaaay faster =))

--- rawgrep.sh
+++ rawgrep.sh
@@ -5,18 +5,18 @@ producerLog=$2
 outFolder=$3
 
 echo "retrieveing expressed interests (interests-timed.log)..."
-cat $consumerLog | grep "express" | grep "STAT" | time-diff.sh > "${outFolder}/interests-timed.log"
+cat $consumerLog | grep "express" | grep "STAT" | time-diff.py > "${outFolder}/interests-timed.log"
 
 echo "retrieveing received data (data-timed.log)..."
-cat $consumerLog | grep "data /" | grep "STAT" | time-diff.sh > "${outFolder}/data-timed.log"
+cat $consumerLog | grep "data /" | grep "STAT" | time-diff.py > "${outFolder}/data-timed.log"
 
 echo "retrieveing rebufferings (rebuf.log)..."
 rebuf.py $consumerLog > "${outFolder}/rebuf.log"
 
 echo "retrieveing producer publishing (sent-or-cached.log)..."
-cat $producerLog | grep "sent\|added" | time-diff.sh > "${outFolder}/sent-or-cached.log"
+cat $producerLog | grep "sent\|added" | time-diff.py > "${outFolder}/sent-or-cached.log"
 
 echo "retrieveing producer incoming interests (interest-and-sent.log)..."
-cat $producerLog | grep "sent\|incoming" | time-diff.sh > "${outFolder}/interest-and-sent.log"
+cat $producerLog | grep "sent\|incoming" | time-diff.py > "${outFolder}/interest-and-sent.log"
 
 echo "done."
---
+++ time-diff.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+
+def getTimestamp(line):
+	try:
+		ts = float(line.split("\t")[0])
+	except:
+		return None
+	return ts
+
+threshold=7
+prevLine = None
+for line in sys.stdin:
+	if prevLine:
+		sys.stdout.write(prevLine)
+
+		tsPrevMs = getTimestamp(prevLine)
+		tsCurMs = getTimestamp(line)
+		if tsPrevMs and tsCurMs:
+			diffMs = (tsCurMs - tsPrevMs)
+			if diffMs > threshold:
+				diffSec = diffMs/1000
+				sys.stdout.write("\t^ {0}sec {1}ms\n".format(diffSec, diffMs))
+	prevLine = line
+	sys.stdout.write(line)
