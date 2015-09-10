ff3f9538e9f18d0feb692f82d41846e11ff89dc5
peetonn <gpeetonn@gmail.com>
2015-09-09 18:10:51 -0700

    updated gethubs.py to parse hub prefixes

--- gethubs.py
+++ gethubs.py
@@ -14,20 +14,38 @@ if '__main__' == __name__:
 		print "usage "+__name__+" <webpage_url>"
 		print "This script parses webpage at <webpage_url> and looks for the table of testbed hubs."
 		print "It is supposed to be testbed status webpage: http://arl.wustl.edu/~jdd/ndnstatus/ndn_prefix/tbs_ndnx.html"
-		eixt()
+		exit(1)
 	page=requests.get(webpage)
 	tree=html.fromstring(page.text)
 	labels=tree.xpath('//table/tr[1]/td/a/font/text()')
 	urls=tree.xpath('//table/tr[1]/td/a/@href')
+	urls2=tree.xpath('//table/tr[position()>1]/td/a/@href')
+	prefixes=tree.xpath('//table/tr[position()>1]/td/a/font/text()')
 	
-	if len(urls) != len(labels):
+	if len(urls) != len(labels) or len(urls2) != len(prefixes):
 		print "something is wrong..."
-		exit()
-	nodes = OrderedDict()
+		exit(1)
+
+	nodeUrls = OrderedDict()
+	nodeLabels = OrderedDict()
+	nodePrefixes = OrderedDict()
+
+	i = 0
+	for url in urls2:
+		m = re.match(r"http://(?P<name>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([0-9A-z-]+)\.?)+)(:80)?",urls[i])
+		if m :
+			nodeUrl = m.group('name')
+			nodePrefix = prefixes[i]
+			nodePrefixes[nodeUrl] = nodePrefix
+		i += 1
+
 	i = 0
 	for label in labels:
 		m = re.match(r"http://(?P<name>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([0-9A-z-]+)\.?)+)(:80)?",urls[i])
-		nodes[label] = m.group('name')
-		print label + "\t" + nodes[label]
+		if m:
+			nodeUrl = m.group('name')
+			nodeUrls[label] = nodeUrl
+			nodeLabels[nodeUrl] = label
+			print label + "\t" + nodeUrls[label] + "\t" + nodePrefixes[nodeUrl]
 		i += 1
 
